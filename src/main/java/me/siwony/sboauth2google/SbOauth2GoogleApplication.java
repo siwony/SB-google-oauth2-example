e297b5da25200243ca235e36fba95668c2f0b4ee
siwony_ <siwon103305@gmail.com>
2022-03-15 16:46:25 +0900

    init

---
+++ src/main/java/me/siwony/sboauth2google/SbOauth2GoogleApplication.java
@@ -0,0 +1,13 @@
+package me.siwony.sboauth2google;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbOauth2GoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbOauth2GoogleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/siwony/sboauth2google/SbOauth2GoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.siwony.sboauth2google;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbOauth2GoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
